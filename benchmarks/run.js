7c01e5e80aaebe9466a2c8f2d9e77b4aa4a53a85
Lee Byron <lee@leebyron.com>
2014-09-02 15:16:48 -0700

    get bench out of package

--- benchmarks/run.js
+++ benchmarks/run.js
@@ -1,15 +1,16 @@
+var async = require('async');
 var benchmark = require('benchmark');
+var exec = require('child_process').exec;
 var pad = require('pad');
 
-function log() {
+function logResult() {
   console.log(this.name);
   this.forEach(function (results) {
     console.log(
-      pad(results.name, 30) + ': ' +
+      pad(results.name, 12) + ': ' +
       pad(15, results.hz.toFixed(2)) + ' +/- ' + results.stats.rme.toFixed(2) + '% op/s'
     );
   });
-  console.log('\n');
 }
 
 global.requireMaybe = function (moduleName) {
@@ -19,6 +20,25 @@ global.requireMaybe = function (moduleName) {
   return null;
 };
 
-require('./map')([10, 100, 1000, 10000, 100000]).forEach(function (suite) {
-  suite.on('complete', log).run(true);
-});
+async.eachLimit(
+  [
+    [ "underscore", "^1.7.0"],
+    [ "lazy.js", "^0.3.2"],
+  ], 4,
+  function (dep, done) {
+    if (requireMaybe(dep[0])) {
+      done();
+    } else {
+      console.log('installing', dep.join('@'));
+      exec('npm install ' + dep.join('@'), done);
+    }
+  },
+  function (err) {
+    if (err) return console.error(err);
+
+    require('./map')([10, 100, 1000, 10000, 100000]).forEach(function (suite) {
+      suite.on('complete', logResult).run(true);
+    });
+
+  }
+);
