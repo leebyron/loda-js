93307cd1ad5b9e4ee4de6c691d4092e2b9d05247
Lee Byron <lee@leebyron.com>
2014-09-02 20:17:33 -0700

    benchmarks

--- benchmarks/run.js
+++ benchmarks/run.js
@@ -16,6 +16,9 @@ function square(x) {
 
 runBenchmarks({
   libraries: {
+    native: {
+      name: '../',
+    },
     loda: {
       name: '../',
     },
@@ -28,28 +31,64 @@ runBenchmarks({
     }
   },
   tests: {
-    Map: {
+
+    'map -> array': {
       variants: {
         10: [square, makeArray(10)],
         100: [square, makeArray(100)],
         1000: [square, makeArray(1000)],
         10000: [square, makeArray(10000)],
       },
+      native: function (_, mapper, input) {
+        return function () {
+          input.map(mapper);
+        }
+      },
       loda: function (_, mapper, input) {
         return function () {
-          _.array(_.map(mapper, input));
+          _.array(_.map(mapper, input))
         }
       },
       underscore: function (_, mapper, input) {
         return function () {
-          _.map(input, mapper);
+          _.map(input, mapper)
         }
       },
       lazyjs: function(_, mapper, input) {
         return function () {
-          _(input).map(mapper).toArray();
+          _(input).map(mapper).toArray()
+        }
+      }
+    },
+
+    'map -> do': {
+      variants: {
+        10: [square, makeArray(10)],
+        100: [square, makeArray(100)],
+        1000: [square, makeArray(1000)],
+        10000: [square, makeArray(10000)],
+      },
+      native: function (_, mapper, input) {
+        return function () {
+          input.map(mapper).forEach(Function.prototype);
+        }
+      },
+      loda: function (_, mapper, input) {
+        return function () {
+          _.doall(Function.prototype, _.map(mapper, input))
+        }
+      },
+      underscore: function (_, mapper, input) {
+        return function () {
+          _.each(_.map(input, mapper), Function.prototype)
+        }
+      },
+      lazyjs: function(_, mapper, input) {
+        return function () {
+          _(input).map(mapper).each(Function.prototype)
         }
       }
     }
+
   }
 });
